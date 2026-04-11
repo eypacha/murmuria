5d3ae036f1f71e75075e754ccf3a06ea1cb17bd8
Ey Pacha! <pachaguionbajo@gmail.com>
2026-04-11 00:40:35 -0300

    feat: enhance SeedBadge styling with background and padding

--- src/components/SeedBadge.vue
+++ src/components/SeedBadge.vue
@@ -5,12 +5,11 @@ import { DEBUG_MODE } from '../game/config/constants.js'
 
 const worldStore = useWorldStore()
 const seedText = computed(() => String(worldStore.seed ?? ''))
-
 </script>
 
 <template>
   <div v-if="DEBUG_MODE" class="pointer-events-auto">
-    <span class="text-xs font-semibold uppercase tracking-[0.22em] text-black">
+    <span class="text-xs bg-white p-2 font-semibold uppercase tracking-[0.22em] text-black">
       Seed {{ seedText }}
     </span>
   </div>
